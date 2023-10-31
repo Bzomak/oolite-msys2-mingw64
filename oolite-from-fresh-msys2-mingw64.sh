ff28432761eb2861b17eea554939af9532d9a5f4
Robert Horswell <r.j.horswell@googlemail.com>
2023-10-31 15:35:01 +0000

    Use Openstep format for libs-base user defaults

--- oolite-from-fresh-msys2-mingw64.sh
+++ oolite-from-fresh-msys2-mingw64.sh
@@ -45,6 +45,10 @@ git clone https://github.com/gnustep/libs-base.git
 
 # Make and install libs-base
 cd libs-base
+
+# Use OpenStep plist format
+sed -i '330 s/NSPropertyListXMLFormat_v1_0/NSPropertyListOpenStepFormat/' Source/NSUserDefaults.m
+
 ./configure
 make -j $(nproc)
 make -j $(nproc) install
