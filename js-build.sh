c182b6c63c0eedb18056c33ec0f757a050d97694
Robert Horswell <r.j.horswell@googlemail.com>
2023-11-29 10:58:09 +0000

    Try to refer to own file in config.mk

--- js-build.sh
+++ js-build.sh
@@ -6,6 +6,8 @@
 
 ###############################
 
+cp msys2-ming264.mk mozilla-2.0/js/src/ref-config/
 cd mozilla-2.0/js/src
+sed -i '106 s/$(OS_ARCH)$(OS_OBJTYPE)$(OS_RELEASE)/msys2-ming64/' config.mk
 ./configure
 make -j$(nproc)
