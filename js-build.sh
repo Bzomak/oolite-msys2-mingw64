f1a70f97a542bd9c9c26907eee05f6d1377a7c9e
Robert Horswell <r.j.horswell@googlemail.com>
2023-11-29 11:03:30 +0000

    Correct typo

--- js-build.sh
+++ js-build.sh
@@ -6,7 +6,7 @@
 
 ###############################
 
-cp msys2-ming264.mk mozilla-2.0/js/src/ref-config/
+cp msys2-mingw64.mk mozilla-2.0/js/src/ref-config/
 cd mozilla-2.0/js/src
 sed -i '106 s/$(OS_ARCH)$(OS_OBJTYPE)$(OS_RELEASE)/msys2-ming64/' config.mk
 ./configure
