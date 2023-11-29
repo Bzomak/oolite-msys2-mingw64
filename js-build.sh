393a4e12043e663043c7d2fa41f69fdad1293f47
Robert Horswell <r.j.horswell@googlemail.com>
2023-11-29 11:25:26 +0000

    Try different version

--- js-build.sh
+++ js-build.sh
@@ -6,8 +6,8 @@
 
 ###############################
 
-cp msys2-mingw64.mk mozilla-2.0/js/src/ref-config/WINNT5.1.mk
+cp msys2-mingw64.mk mozilla-2.0/js/src/ref-config/WINNT6.0.mk
 cd mozilla-2.0/js/src
-sed -i '106 s/$(OS_ARCH)$(OS_OBJTYPE)$(OS_RELEASE)/WINNT5.1/' config.mk
+sed -i '106 s/$(OS_ARCH)$(OS_OBJTYPE)$(OS_RELEASE)/WINNT6.0/' config.mk
 ./configure
 make -j$(nproc)
