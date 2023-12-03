b7e321284893ba3fd713044b64bd970e46eef4d4
Robert Horswell <r.j.horswell@googlemail.com>
2023-12-03 18:36:54 +0000

    Try replacing all current WINNT#.#.mk files

--- js-build.sh
+++ js-build.sh
@@ -6,9 +6,11 @@
 
 ###############################
 
+cp msys2-mingw64.mk mozilla-2.0/js/src/ref-config/WINNT4.0.mk
+cp msys2-mingw64.mk mozilla-2.0/js/src/ref-config/WINNT5.0.mk
+cp msys2-mingw64.mk mozilla-2.0/js/src/ref-config/WINNT5.1.mk
+cp msys2-mingw64.mk mozilla-2.0/js/src/ref-config/WINNT5.2.mk
 cp msys2-mingw64.mk mozilla-2.0/js/src/ref-config/WINNT6.0.mk
-ls -al mozilla-2.0/js/src/ref-config/
-cat mozilla-2.0/js/src/ref-config/WINNT6.0.mk
 cd mozilla-2.0/js/src
 sed -i '106 s/$(OS_ARCH)$(OS_OBJTYPE)$(OS_RELEASE)/WINNT6.0/' config.mk
 ./configure
