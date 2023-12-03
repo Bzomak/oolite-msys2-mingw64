c4909ea9b1c593a35d1a02f2ab6d0cee21c3e9c5
Robert Horswell <r.j.horswell@googlemail.com>
2023-12-03 18:28:42 +0000

    Try testing to see if modified file exists

--- js-build.sh
+++ js-build.sh
@@ -7,6 +7,8 @@
 ###############################
 
 cp msys2-mingw64.mk mozilla-2.0/js/src/ref-config/WINNT6.0.mk
+ls -al mozilla-2.0/js/src/ref-config/
+cat mozilla-2.0/js/src/ref-config/WINNT6.0.mk
 cd mozilla-2.0/js/src
 sed -i '106 s/$(OS_ARCH)$(OS_OBJTYPE)$(OS_RELEASE)/WINNT6.0/' config.mk
 ./configure
