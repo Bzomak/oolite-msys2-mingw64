166f9c4ebb12b6cb64a2616800e1baed2c22fa25
Robert Horswell <r.j.horswell@googlemail.com>
2023-12-03 19:10:24 +0000

    Try sedding v10.0

--- js-build.sh
+++ js-build.sh
@@ -14,6 +14,6 @@ cp msys2-mingw64.mk mozilla-2.0/js/src/ref-config/WINNT5.2.mk
 cp msys2-mingw64.mk mozilla-2.0/js/src/ref-config/WINNT6.1.mk
 cp msys2-mingw64.mk mozilla-2.0/js/src/ref-config/WINNT10.0.mk
 cd mozilla-2.0/js/src
-#sed -i '106 s/$(OS_ARCH)$(OS_OBJTYPE)$(OS_RELEASE)/WINNT6.0/' config.mk
+sed -i '106 s/$(OS_ARCH)$(OS_OBJTYPE)$(OS_RELEASE)/WINNT10.0/' config.mk
 ./configure
 make -j$(nproc)
