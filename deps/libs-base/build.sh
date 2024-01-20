4fba4d5db97d398968d58a217d25323849398580
Robert Horswell <r.j.horswell@googlemail.com>
2024-01-20 19:41:23 +0000

    Use vars to build libs-base in script

--- deps/libs-base/build.sh
+++ deps/libs-base/build.sh
@@ -8,6 +8,7 @@
 
 . /mingw64/share/GNUstep/Makefiles/GNUstep.sh
 cd libs-base
+# Use OpenStep plist format
 sed -i '330 s/NSPropertyListXMLFormat_v1_0/NSPropertyListOpenStepFormat/' Source/NSUserDefaults.m
 ./configure
 make -j$(nproc)
