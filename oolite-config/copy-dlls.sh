956673a0f9d5b11374f9278689ffd3b415046f33
Robert Horswell <r.j.horswell@googlemail.com>
2024-01-31 21:28:40 +0000

    Add more dlls

--- oolite-config/copy-dlls.sh
+++ oolite-config/copy-dlls.sh
@@ -12,3 +12,11 @@ cp /mingw64/bin/libopenal-1.dll ./oolite/oolite.app/
 cp /mingw64/bin/libpng16-16.dll ./oolite/oolite.app/
 cp /mingw64/bin/SDL.dll ./oolite/oolite.app/
 cp /mingw64/bin/libvorbisfile-3.dll ./oolite/oolite.app/
+cp /mingw64/bin/libgcc_s_seh-1.dll ./oolite/oolite.app/
+cp /mingw64/bin/libwinpthread-1.dll ./oolite/oolite.app/
+
+###############################
+
+# Copy the js lib from the oolite-windows-dependencies repo to the oolite.app folder
+# Once we can build it ourselves it can be copied with the other dlls
+cp ./oolite/deps/Windows-deps/x86_64/DLLs/js32ECMAv5.dll ./oolite/oolite.app/
