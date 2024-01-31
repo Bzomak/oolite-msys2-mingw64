2b1e127cedfbb97fb653ae1ec2bb9a15f1425499
Robert Horswell <r.j.horswell@googlemail.com>
2024-01-31 21:07:41 +0000

    Add more dlls

--- oolite-config/copy-dlls.sh
+++ oolite-config/copy-dlls.sh
@@ -10,3 +10,5 @@ cp /mingw64/bin/libobjc-4.dll ./oolite/oolite.app/
 cp /mingw64/bin/gnustep-base-1_29.dll ./oolite/oolite.app/
 cp /mingw64/bin/libopenal-1.dll ./oolite/oolite.app/
 cp /mingw64/bin/libpng16-16.dll ./oolite/oolite.app/
+cp /mingw64/bin/SDL.dll ./oolite/oolite.app/
+cp /mingw64/bin/libvorbisfile-3.dll ./oolite/oolite.app/
