d81e008239497efa902488d15033aebe314716f7
Robert Horswell <r.j.horswell@googlemail.com>
2024-02-28 11:13:36 +0000

    Try to fix compile errors in synthesize.cpp

--- deps/espeak/build.sh
+++ deps/espeak/build.sh
@@ -21,12 +21,95 @@ mv ./espeak-1.43.03-source/src/portaudio19.h ./espeak-1.43.03-source/src/portaud
 # Copy the file speak_lib.h from <espeakSourceFolder>/platforms/windows/windows_dll/src to <espeakSourceFolder>/src.
 cp ./espeak-1.43.03-source/platforms/windows/windows_dll/src/speak_lib.h ./espeak-1.43.03-source/src/
 
-# Fix compile error
+# Fix compile errors
+
 # dictionary.cpp: In function 'void InitGroups(Translator*)':
 # dictionary.cpp:163:48: error: cast from 'char*' to 'long int' loses precision [-fpermissive]
 #   163 |                         pw = (unsigned int *)(((long)p+4) & ~3);  // advance to next word boundary
 #       |                                                ^~~~~~~
 sed -i '163 s/long/uintptr_t/' ./espeak-1.43.03-source/src/dictionary.cpp
+
+# synthesize.cpp: In function 'void DoAmplitude(int, unsigned char*)':
+# synthesize.cpp:152:16: error: cast from 'unsigned char*' to 'long int' loses precision [-fpermissive]
+#   152 |         q[2] = (long)amp_env;
+#       |                ^~~~~~~~~~~~~
+# synthesize.cpp: In function 'void DoPitch(unsigned char*, int, int)':
+# synthesize.cpp:181:16: error: cast from 'unsigned char*' to 'long int' loses precision [-fpermissive]
+#   181 |         q[2] = (long)env;
+#       |                ^~~~~~~~~
+# synthesize.cpp: In function 'int DoSample2(int, int, int, int, int, int)':
+# synthesize.cpp:307:24: error: cast from 'unsigned char*' to 'long int' loses precision [-fpermissive]
+#   307 |                 q[2] = long(&wavefile_data[index]);
+#       |                        ^~~~~~~~~~~~~~~~~~~~~~~~~~~
+# synthesize.cpp:328:16: error: cast from 'unsigned char*' to 'long int' loses precision [-fpermissive]
+# g++ -O2 -DUSE_PORTAUDIO -D PATH_ESPEAK_DATA=\"Resources\" -Id:/msys_x/1.0/mingw/include/pthreads -Wall -pedantic \
+#   328 |         q[2] = long(&wavefile_data[index]);
+# -I. -D LIBRARY -c -fno-exceptions  voices.cpp  -o x_voices.o
+#       |                ^~~~~~~~~~~~~~~~~~~~~~~~~~~
+# synthesize.cpp:343:24: error: cast from 'unsigned char*' to 'long int' loses precision [-fpermissive]
+#   343 |                 q[2] = long(&wavefile_data[index+x]);
+#       |                        ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# synthesize.cpp:359:24: error: cast from 'unsigned char*' to 'long int' loses precision [-fpermissive]
+#   359 |                 q[2] = long(&wavefile_data[index+x]);
+#       |                        ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# synthesize.cpp: In function 'void SmoothSpect()':
+# synthesize.cpp:814:40: error: cast from 'frame_t*' to 'long int' loses precision [-fpermissive]
+#   814 |                                 q[3] = (long)frame2;
+#       |                                        ^~~~~~~~~~~~
+# synthesize.cpp:862:48: error: cast from 'frame_t*' to 'long int' loses precision [-fpermissive]
+#   862 |                                         q[2] = (long)frame2;
+#       |                                                ^~~~~~~~~~~~
+# synthesize.cpp:873:48: error: cast from 'frame_t*' to 'long int' loses precision [-fpermissive]
+#   873 |                                         q[2] = (long)frame2;
+#       |                                                ^~~~~~~~~~~~
+# synthesize.cpp:904:48: error: cast from 'frame_t*' to 'long int' loses precision [-fpermissive]
+#   904 |                                         q[2] = (long)frame2;
+#       |                                                ^~~~~~~~~~~~
+# synthesize.cpp:946:48: error: cast from 'frame_t*' to 'long int' loses precision [-fpermissive]
+#   946 |                                         q[3] = (long)frame2;
+#       |                                                ^~~~~~~~~~~~
+# synthesize.cpp:957:48: error: cast from 'frame_t*' to 'long int' loses precision [-fpermissive]
+#   957 |                                         q[3] = (long)frame2;
+#       |                                                ^~~~~~~~~~~~
+# synthesize.cpp: In function 'int DoSpect2(PHONEME_TAB*, int, FMT_PARAMS*, PHONEME_LIST*, int)':
+# synthesize.cpp:1061:48: error: cast from 'frame_t*' to 'long int' loses precision [-fpermissive]
+#  1061 |                         wcmdq[last_wcmdq][3] = (long)frame1;
+#       |                                                ^~~~~~~~~~~~
+# synthesize.cpp:1073:56: error: cast from 'frame_t*' to 'long int' loses precision [-fpermissive]
+#  1073 |                                 wcmdq[last_wcmdq][3] = (long)fr;
+#       |                                                        ^~~~~~~~
+# synthesize.cpp:1138:40: error: cast from 'frame_t*' to 'long int' loses precision [-fpermissive]
+#  1138 |                                 q[2] = long(frame1);
+#       |                                        ^~~~~~~~~~~~
+# synthesize.cpp:1139:40: error: cast from 'frame_t*' to 'long int' loses precision [-fpermissive]
+#  1139 |                                 q[3] = long(frame2);
+#       |                                        ^~~~~~~~~~~~ 
+# synthesize.cpp: In function 'void DoVoiceChange(voice_t*)':
+# synthesize.cpp:1175:32: error: cast from 'voice_t*' to 'long int' loses precision [-fpermissive]
+#  1175 |         wcmdq[wcmdq_tail][1] = (long)(v2);
+#       |                                ^~~~~~~~~~
+# synthesize.cpp: In function 'void DoEmbedded(int&, int)':
+# synthesize.cpp:1207:64: error: cast from 'char*' to 'long int' loses precision [-fpermissive]
+#  1207 |                                         wcmdq[wcmdq_tail][2] = (long)soundicon_tab[value].data + 44;  // skip WAV header
+#       |                                                                ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+sed -i '152 s/long/uintptr_t/' ./espeak-1.43.03-source/src/synthesize.cpp
+sed -i '181 s/long/uintptr_t/' ./espeak-1.43.03-source/src/synthesize.cpp
+sed -i '307 s/long/uintptr_t/' ./espeak-1.43.03-source/src/synthesize.cpp
+sed -i '328 s/long/uintptr_t/' ./espeak-1.43.03-source/src/synthesize.cpp
+sed -i '343 s/long/uintptr_t/' ./espeak-1.43.03-source/src/synthesize.cpp
+sed -i '359 s/long/uintptr_t/' ./espeak-1.43.03-source/src/synthesize.cpp
+sed -i '814 s/long/uintptr_t/' ./espeak-1.43.03-source/src/synthesize.cpp
+sed -i '862 s/long/uintptr_t/' ./espeak-1.43.03-source/src/synthesize.cpp
+sed -i '873 s/long/uintptr_t/' ./espeak-1.43.03-source/src/synthesize.cpp
+sed -i '904 s/long/uintptr_t/' ./espeak-1.43.03-source/src/synthesize.cpp
+sed -i '946 s/long/uintptr_t/' ./espeak-1.43.03-source/src/synthesize.cpp
+sed -i '957 s/long/uintptr_t/' ./espeak-1.43.03-source/src/synthesize.cpp
+sed -i '1061 s/long/uintptr_t/' ./espeak-1.43.03-source/src/synthesize.cpp
+sed -i '1073 s/long/uintptr_t/' ./espeak-1.43.03-source/src/synthesize.cpp
+sed -i '1138 s/long/uintptr_t/' ./espeak-1.43.03-source/src/synthesize.cpp
+sed -i '1139 s/long/uintptr_t/' ./espeak-1.43.03-source/src/synthesize.cpp
+sed -i '1175 s/long/uintptr_t/' ./espeak-1.43.03-source/src/synthesize.cpp
+sed -i '1207 s/long/uintptr_t/' ./espeak-1.43.03-source/src/synthesize.cpp
 
 # Build eSpeak
 cd espeak-1.43.03-source/src || exit
