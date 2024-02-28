3d7b8f616e587766c50fa0da9c02164a15a6fb31
Robert Horswell <r.j.horswell@googlemail.com>
2024-02-28 17:31:00 +0000

    Change bar lines

--- deps/espeak/build.sh
+++ deps/espeak/build.sh
@@ -130,10 +130,10 @@ sed -i '1207 s/long/uintptr_t/' ./espeak-1.43.03-source/src/synthesize.cpp
 #       |                                 ^
 # 
 #sed -i '29 s/^/\/\//' ./espeak-1.43.03-source/src/speech.h
-#sed -i '29 s/Sleep((x)/1000)/Sleep((DWORD)((x)/1000))/' ./espeak-1.43.03-source/src/speech.h
+sed -i '29 s|Sleep((x)/1000)|Sleep((DWORD)((x)/1000))|' ./espeak-1.43.03-source/src/speech.h
 #sed -i '19 s//#include windows.h/' ./espeak-1.43.03-source/src/event.cpp
 #sed -i 's/Sleep((x)/1000)/nanosleep(x)/' ./espeak-1.43.03-source/src/speech.h
-sed -i 's/^\(#define usleep(x)   Sleep(\)\(.*\)\/1000\)/\1(DWORD)(\2\/1000))/' ./espeak-1.43.03-source/src/speech.h
+#sed -i 's/^\(#define usleep(x)   Sleep(\)\(.*\)\/1000\)/\1(DWORD)(\2\/1000))/' ./espeak-1.43.03-source/src/speech.h
 
 cat ./espeak-1.43.03-source/src/speech.h
 ###############################
